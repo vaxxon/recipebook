540ece9b4a64cab84dfc8c6555bfa0aada85eadc
Vincent Allport <vincentallport@gmail.com>
2025-05-07 23:38:52 -0400

    sample database data

---
+++ inserts.sql
@@ -0,0 +1,20 @@
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('banana', true, true, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('brown sugar', true, true, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('egg', false, true, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('all-purpose flour', true, true, true, false);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('butter', false, true, false, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('baking soda', true, true, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('chicken', false, false, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('buttermilk', false, true, false, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('salt', true, true, true, true);
+insert into ingredients (name, vegan, vegetarian, dairy_free, gluten_free) values ('paprika', true, true, true, true);
+
+insert into tags (name) values ('American');
+insert into tags (name) values ('baking');
+insert into tags (name) values ('dessert');
+insert into tags (name) values ('dinner');
+insert into tags (name) values ('soul food');
+
+insert into recipes (name, steps) values ('Mom''s banana bread', 'Mix the wets. Whisk the drys. Combine wet with dry. Bake at 350 for 35 minutes.');
+insert into recipes (name, steps) values ('Grandma''s banana bread', 'Whisk the wets. Mix the drys. Combine dry with wet. Bake at 375 for 30 minutes.');
+insert into recipes (name, steps) values ('Dad''s fried chicken', 'Whisk the drys. Mix the drys. Dunk chicken. Fry.');
---
+++ inserts_ri.sql
@@ -0,0 +1,20 @@
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (1, 1, 2.33, 'cups', 'medium, mashed'); -- banana
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (2, 1, 0.75, 'cups', 'packed'); -- brown sugar
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, instruction) values (3, 1, 2, 'large, beaten'); -- egg
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (4, 1, 2, 'cups'); -- ap flour
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (5, 1, .5, 'cups', 'melted'); -- butter
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (6, 1, 1, 'tsp'); -- baking soda
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (9, 1, 0.25, 'tsp'); -- salt
+
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (1, 2, 2.33, 'cups', 'medium, mashed'); -- banana
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (2, 2, 0.75, 'cups', 'packed'); -- brown sugar
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, instruction) values (3, 2, 2, 'jumbo, beaten'); -- egg
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (4, 2, 2, 'cups'); -- ap flour
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (5, 2, .5, 'cups', 'browned'); -- butter
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (6, 2, 1, 'tsp'); -- baking soda
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (9, 2, 0.25, 'tsp'); -- salt
+
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom, instruction) values (7, 3, 3, 'lbs', 'bone-in, skin-on'); -- chicken
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (8, 3, 2, 'cups'); -- buttermilk
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (9, 3, 1, 'tsp'); -- salt
+insert into recipes_ingredients (ingredient_id, recipe_id, measure, uom) values (10, 3, 0.5, 'tsp'); -- paprika
