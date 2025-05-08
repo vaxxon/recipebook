4f198173a776176275341d181ad8f09505785ead
Vincent Allport <vincentallport@gmail.com>
2025-05-08 00:08:08 -0400

    format homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Recipe Book'})
+const Recipe = require('../models/recipe')
+const Tag = require('../models/tag')
+const Ingredient = require('../models/ingredient')
+
+router.get('/', async (req, res, next) => {
+    const recipes = await Recipe.all()
+    const tags = await Tag.all()
+    const ingredients = await Ingredient.all()
+    res.render('index', {title: 'Recipe Book', recipes: recipes, tags: tags, ingredients: ingredients})
 })
 
 module.exports = router;
